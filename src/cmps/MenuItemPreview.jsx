23a371e91b34d2b6c2029c07435692fb9899732d
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-02-12 13:38:27 +0200

    Render all the menu data in the MenuItemPreview cmp

--- src/cmps/MenuItemPreview.jsx
+++ src/cmps/MenuItemPreview.jsx
@@ -1,5 +1,15 @@
 export function MenuItemPreview({ item }) {
+  const { title, catagory, price, img, desc } = item
   return (
-    <h1>{item.title}</h1>
+    <article className='menu-item'>
+      <img className='img' src={img} alt={title} />
+      <div className="item-info">
+        <header>
+          <h5>{title}</h5>
+          <span className='item-price'>{price}</span>
+        </header>
+        <p className='item-text'>{desc}</p>
+      </div>
+    </article>
   )
 } 
