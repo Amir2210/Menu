657179a2c8b52525b28ff488a3eec07fed43d873
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-02-12 13:20:44 +0200

    Fetch the menu data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import { useState } from 'react'
+import data from './data'
 const App = () => {
+  const [menuItems, setMenuItems] = useState(data)
   return <h2>Menu Starter</h2>
 }
 export default App
